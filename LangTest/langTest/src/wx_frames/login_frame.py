fe0eb2eb4d251fdb65525848ad0d327268274336
Simon Albino <simon.d.albino@gmail.com>
2014-07-06 15:59:28 +0200

    Improvements to combobox in login frame

--- LangTest/langTest/src/wx_frames/login_frame.py
+++ LangTest/langTest/src/wx_frames/login_frame.py
@@ -3,18 +3,17 @@ import wx
 
 class LoginFrame(wx.Frame):
     def __init__(self):
-        self.clear_on_text_changed = False
+        self.no_ignore_OnTextChange = False
         self.app = wx.App(0)
-        super(LoginFrame, self).__init__(None, title="Login", size=(200,100))
-        panel = wx.Panel(self, -1)
-        box = wx.BoxSizer(wx.HORIZONTAL)
-        self.cb = wx.ComboBox(panel, -1, 'User name', style = wx.CB_DROPDOWN)
-        self.cb.SetEditable(True)
-        box.Add(self.cb)
+        super(LoginFrame, self).__init__(None, title="Login", size=(200,300))
+        self.panel = wx.Panel(self, -1)
+        self.box = wx.BoxSizer(wx.HORIZONTAL)
+        self.cb = wx.ComboBox(self.panel, -1, 'User name', size=(100,100), style = wx.CB_SIMPLE | wx.CB_DROPDOWN)
+        self.box.Add(self.cb)
         self.Bind(wx.EVT_TEXT, self.OnTextChanged, self.cb) 
-        button = wx.Button(panel, -1, 'Login')
-        box.Add(button, 1 )
-        panel.SetSizer(box)
+        button = wx.Button(self.panel, -1, 'Login')
+        self.box.Add(button, 1 )
+        self.panel.SetSizer(self.box)
         self.Centre()
         
     def start(self, login):
@@ -26,17 +25,17 @@ class LoginFrame(wx.Frame):
         return True
     
     def OnTextChanged(self, event):
-        if not self.clear_on_text_changed:
-            self.clear_on_text_changed = True
+        if not self.no_ignore_OnTextChange:
             self.text_ctrl = self.cb.GetValue()
             self.cb.Clear()
+            self.no_ignore_OnTextChange = True
             self.cb.SetValue(self.text_ctrl)
             for item in self.login.receive_partial_text(event.GetString()):
                 self.cb.Append(item)
             self.cb.SetInsertionPointEnd()
-            self.cb.Popup()
+            self.panel.SetSizer(self.box)
         else:
-            self.clear_on_text_changed = False
+            self.no_ignore_OnTextChange = False
     
     def close(self):
         self.Close()
